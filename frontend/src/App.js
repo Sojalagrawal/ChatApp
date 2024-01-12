1b62dca6ac89a9d184f1384b326a2c025362f250
Sojal agrawal <sojalagrawal2@gmail.com>
2024-01-12 17:11:25 +0530

    fixing some issue2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import { ToastContainer } from 'react-toastify';
-import ChatPage from "./Pages/ChatPage";
+// import ChatPage from "./Pages/ChatPage";
 import SignUp from "./components/Authentication/SignUp";
 import Login from "./components/Authentication/Login";
+import SideDrawer from "./components/Miscellaneous/SideDrawer";
+
 import 'react-toastify/dist/ReactToastify.css';
 import ChatProvider from './context/ChatProvider';
 
@@ -19,7 +21,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/chats" element={<ChatPage/>}/>
+                <Route path="/chats" element={<SideDrawer/>}/>
                 <Route path="/signup" element={<SignUp/>}/>
             </Routes>
             <ToastContainer theme="dark"/>
--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,14 +1,14 @@
-import React, { useEffect,useState} from 'react';
-import { ChatState } from '../context/ChatProvider';
-import SideDrawer from "../components/Miscellaneous/SideDrawer";
+// import React, { useEffect,useState} from 'react';
+// import { ChatState } from '../context/ChatProvider';
+// import SideDrawer from "../components/Miscellaneous/SideDrawer";
 
 
 
-export default function ChatPage() {
- const user=localStorage.getItem("userinfo")?JSON.parse(localStorage.getItem("userinfo")):"";
-  return (
-    <div style={{width:"100%"}}>
-      {user && <SideDrawer/>}
-    </div>
-  )
-}
+// export default function ChatPage() {
+//  const user=localStorage.getItem("userinfo")?JSON.parse(localStorage.getItem("userinfo")):"";
+//   return (
+//     <div style={{width:"100%"}}>
+//       {user && <SideDrawer/>}
+//     </div>
+//   )
+// }
